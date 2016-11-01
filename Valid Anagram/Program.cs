48802f2c198c7e39ce0a6753c0bd6e0ceaeb9767
SVEN-HSU <sven.hsu@gmail.com>
2016-11-02 00:32:54 +0800

    20161102

---
+++ Valid Anagram/Program.cs
@@ -0,0 +1,45 @@
+﻿// https://leetcode.com/problems/valid-anagram/
+// Accepted
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isAnagram("anagram", "naamagr"));
+            Console.Read();
+        }
+
+        public static bool isAnagram(string s, string t)
+        {
+            if (s == null)
+            {
+                return false;
+            }
+
+            if (t == null)
+            {
+                return false;
+            }
+
+            if (s.Length == 0 || t.Length == 0)
+            {
+                if (s.Length == 0 && t.Length == 0)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+
+            return s.OrderBy(x => x).ToArray().SequenceEqual(t.OrderBy(x => x).ToArray());
+        }
+    }
+}
